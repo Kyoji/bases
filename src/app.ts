4d1ff3d20f835da5cfdd306d42e16a255158d97e
Daniel Owens <daniel@pns-mgmt.com>
2019-06-26 14:23:52 -0400

    Update .gitignore

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,7 @@ class App {
     constructor() {
         this.baseTo = new Base(16, this.bytes);
         this.baseFrom = new Base(10, this.bytes);
-        this.baseFrom.insert('1234');
+        this.baseFrom.insert('87392');
         this.baseTo.convert(this.baseFrom);
     }
 }
