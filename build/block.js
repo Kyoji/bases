4b51fecf57817831a014b127a60ae7de6292878e
Daniel Owens <kyoji1@gmail.com>
2019-07-11 16:31:57 -0400

    Bits working!

--- build/block.js
+++ build/block.js
@@ -4,30 +4,42 @@
         if (v !== undefined) module.exports = v;
     }
     else if (typeof define === "function" && define.amd) {
-        define(["require", "exports"], factory);
+        define(["require", "exports", "./baseStringMap"], factory);
     }
 })(function (require, exports) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
+    const baseStringMap_1 = require("./baseStringMap");
     class Block {
         constructor(multiplier) {
+            this.bits = [];
             this.multiplier = multiplier;
             this.contains = 0;
             this.createHTML();
         }
         createHTML() {
             this.blockContainer = document.createElement('div');
+            this.bitContainer = document.createElement('div');
+            this.bitContainer.classList.add('bit-container');
             this.blockElement = document.createElement('div');
             this.blockContainer.classList.add('block-container');
             this.blockElement.classList.add('block');
             this.blockElement.classList.add('zero');
             this.blockElement.innerHTML = '0';
+            this.blockContainer.append(this.bitContainer);
             this.blockContainer.append(this.blockElement);
+            // this.bits[0] = document.createElement('div') as HTMLDivElement;
+            // this.bits[1] = document.createElement('div') as HTMLDivElement;
+            // this.bits[0].classList.add('bit');
+            // this.bits[1].classList.add('bit');
+            // this.bitContainer.append(this.bits[0]);
+            // this.bitContainer.append(this.bits[1]);
         }
         update(number, isZero) {
-            this.contains = +number;
+            this.contains = baseStringMap_1.default.get(number);
             this.addZero(isZero);
             this.blockElement.innerHTML = number;
+            this.syncBits();
         }
         addZero(isZero) {
             switch (isZero) {
@@ -39,6 +51,18 @@
                     break;
                 default:
                     break;
+            }
+        }
+        syncBits() {
+            this.bits.forEach((elem, i) => {
+                this.bitContainer.removeChild(elem);
+            });
+            this.bits.length = 0;
+            let i = 0;
+            for (i; i < this.contains; i++) {
+                this.bits[i] = document.createElement('div');
+                this.bits[i].classList.add('bit');
+                this.bitContainer.append(this.bits[i]);
             }
         }
     }
--- src/block.ts
+++ src/block.ts
@@ -1,8 +1,12 @@
+import baseStringMap from './baseStringMap';
+
 export default class Block {
     public multiplier: number;
     public contains: number;
     public blockContainer: HTMLDivElement;
     private blockElement: HTMLDivElement;
+    private bitContainer: HTMLDivElement;
+    private bits: HTMLDivElement[] = [];
 
     constructor(multiplier: number) {
         this.multiplier = multiplier;
@@ -12,18 +16,28 @@ export default class Block {
 
     private createHTML() {
         this.blockContainer = document.createElement('div') as HTMLDivElement;
+        this.bitContainer = document.createElement('div') as HTMLDivElement;
+        this.bitContainer.classList.add('bit-container');
         this.blockElement =  document.createElement('div') as HTMLDivElement;
         this.blockContainer.classList.add('block-container');
         this.blockElement.classList.add('block');
         this.blockElement.classList.add('zero');
         this.blockElement.innerHTML = '0';
+        this.blockContainer.append(this.bitContainer);
         this.blockContainer.append(this.blockElement);
+        // this.bits[0] = document.createElement('div') as HTMLDivElement;
+        // this.bits[1] = document.createElement('div') as HTMLDivElement;
+        // this.bits[0].classList.add('bit');
+        // this.bits[1].classList.add('bit');
+        // this.bitContainer.append(this.bits[0]);
+        // this.bitContainer.append(this.bits[1]);
     }
 
     public update(number: string, isZero: boolean) {
-        this.contains = +number;
+        this.contains = baseStringMap.get(number);
         this.addZero(isZero);
         this.blockElement.innerHTML = number;
+        this.syncBits();
     }
 
     public addZero(isZero: boolean) {
@@ -38,6 +52,19 @@ export default class Block {
                 break;
         }
         
+    }
+
+    private syncBits() {
+        this.bits.forEach((elem, i) => {
+            this.bitContainer.removeChild(elem);
+        })
+        this.bits.length = 0;
+        let i = 0;
+        for(i; i < this.contains; i++) {
+            this.bits[i] = document.createElement('div');
+            this.bits[i].classList.add('bit');
+            this.bitContainer.append(this.bits[i]);
+        }
     }
 
 }
